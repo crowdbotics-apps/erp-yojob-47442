0a45d145e7cea465a21facaff1c0603c788c9d78
Crowdbotics <team@crowdbotics.com>
2024-04-10 10:03:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "erp-yojob-47442",
+  url: "https://erp-yojob-47442.botics.co",
+  api: "https://erp-yojob-47442.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
